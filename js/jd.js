af8fb2811234352d692f39fb7e6eec9aec1e9977
zhang hang <zhanghang.org@gmail.com>
2016-09-14 15:03:47 +0800

    自动提交

--- js/jd.js
+++ js/jd.js
@@ -8,7 +8,9 @@ console.log("个人主页：http://zhanghang.org");
 
 var code = "<div id='qp_div'>"
             + "最高出价<input type='text' id='qp_max_price' />&nbsp;&nbsp;&nbsp;&nbsp;"
+            + "单次加价<input type='text' id='qp_add_price' value='1' />&nbsp;&nbsp;&nbsp;&nbsp;"
         + "<input type='button' value='后台抢拍' id='qp_btn_begin' class='qp_btn'/>&nbsp;&nbsp;&nbsp;&nbsp;"
+        + "<input type='checkbox' id='qp_chb_auth' class='qp_btn'/>全自动&nbsp;&nbsp;&nbsp;&nbsp;"
         + "【请在拍卖剩余时间最后几秒狂点“后台抢拍”按钮】<span id='say' style='color:red;'></span></div>";
 $('body').prepend(code);
 
@@ -16,10 +18,12 @@ $('body').prepend(code);
 var num = queryNum();
 // 取商品售价
 //var priceSale = queryPriceSale();
+var int = 0;
+var remainTime = 0;
 
 
 $('#qp_btn_begin').on('click', function(){
-    queryPriceCurrent(num)
+    runjob(num);
 });
 
 
@@ -39,23 +43,30 @@ function queryPriceSale(){
     return priceSale
 }
 
-function queryPriceCurrent(num) {
+function runjob(num) {
     var host = "http://dbditem.jd.com"
     var currentInterface = host + "/services/currentList.action?paimaiIds="+num+"&callback=showData&t=1432893946478&callback=jQuery8717195&_=1432893946480"
     $.get(currentInterface, function(data) {
         var start = data.indexOf('[')
         var end = data.lastIndexOf(']') + 1
         data = data.substring(start, end)
-
         var objs = $.parseJSON(data)
+        remainTime = objs[0].remainTime/1000
         var priceCurrent = objs[0].currentPrice
-        console.info("商品当前报价："+priceCurrent)
-        // 拍卖报价
-        bid(num, priceCurrent * 1 + 2)
+
+        console.info("商品当前报价："+priceCurrent + "剩余时间："+remainTime)
+        if (int != 0) {
+            window.clearInterval(int);
+        }
+        int = setInterval("timeMsg(remainTime)", 200)
+        var addPrice = $('#qp_add_price').val()*1.00
+        bid(num, priceCurrent * 1 + addPrice)
     });
 }
 
+
 function bid(paimaiId, price) {
+    console.info("抢拍中：" + price)
     var max = $('#qp_max_price').val()
     if (price*1.00 < max*1.00) {
         var url = "/services/bid.action?t=" + getRamdomNumber();
@@ -85,6 +96,16 @@ function getRamdomNumber(){
     return num;
 }
 
+function timeMsg(time) {
+    if (remainTime>0){
+        var auto = $('#qp_chb_auth').is(':checked');
+        if (remainTime<0.9 && auto){
+            runjob(num);
+        }
+        console.info("抢拍倒计时：[ " + remainTime + " s]");
+        remainTime = remainTime-0.2;
+    }
+}
 
 
 
